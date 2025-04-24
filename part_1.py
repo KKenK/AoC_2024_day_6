f5b0c863637424b531ce4b685525e8ca54300973
Kyle <kylekenrick55@gmail.com>
2025-04-25 00:21:55 +0100

    add_char_border imported and a char border is added to the parsed input.

--- part_1.py
+++ part_1.py
@@ -2,20 +2,23 @@ from classes import input_parser
 from classes import grid
 from classes import guard
 from modules import find_guard
+from modules import add_char_border
 
-parsed_input = input_parser.InputParser(r"C:\Users\kylek\Documents\code\Advent_of_code\2024\Day_6\input.txt").parsed_input
+parsed_input = input_parser.InputParser(r"C:\Users\kylek\Documents\code\Advent_of_code\2024\Day_6\test.txt").parsed_input
 
-lab = grid.Grid(parsed_input)
+parsed_input_with_border = add_char_border.add_char_border(parsed_input, "O")
 
-lab_guard = guard.Guard("^", find_guard.return_guard_coordinates(parsed_input))
+lab = grid.Grid(parsed_input_with_border)
 
-while lab_guard.has_exited(parsed_input) == False:
+lab_guard = guard.Guard("^", find_guard.return_guard_coordinates(parsed_input_with_border))
+
+while lab_guard.has_exited(parsed_input_with_border) == False:
 
     lab.mark_grid_position(x = lab_guard.current_location_x_coordinate,
                             y = lab_guard.current_location_y_coordinate,
                             mark_char = "X")
 
-    lab_guard.take_one_step_forward(parsed_input)
+    lab_guard.take_one_step_forward(parsed_input_with_border)
 
 distinct_positions_visited_count = 0 
 
